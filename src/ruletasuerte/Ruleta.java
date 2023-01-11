7aa9a510ce081930e88bab23534ce19a49215fa7
luis miguel <sempereluismiguel@gmail.com>
2023-01-11 08:54:05 +0100

    first commit

---
+++ src/ruletasuerte/Ruleta.java
@@ -0,0 +1,50 @@
+package ruletasuerte;
+
+/**
+ *
+ * @author a22luismsg
+ */
+public class Ruleta {
+    
+    /*
+    OPCIONES DE LA RULETA:
+    1: quiebra
+    2: pierde turno
+    3: comodín
+    102: x2
+    12: 1/2
+    */
+    private final static int [] Ruleta = {200, 1, 102, 100, 50, 100, 150, 2, 150, 100, 50, 100, 200, 50, 150, 12, 50, 150, 100, 3, 150, 100, 50, 100};
+    private static int premio = 1;
+    
+    public static void tirar() {
+        int opt = (int)(Math.random()*25);
+        
+        switch (Ruleta[opt]) {
+            case 1:
+                System.out.println("QUIEBRA");
+                break;
+            case 2:
+                System.out.println("PIERDE TURNO");
+                break;
+            case 3:
+                System.out.println("COMODÍN");
+                break;
+            case 102:
+                System.out.println("x2");
+                premio *= 2;
+                break;
+            case 12:
+                System.out.println("1/2");
+                premio /= 2;
+                break;
+            default:
+                premio += Ruleta[opt];
+        }
+        
+        
+        System.out.println(premio);
+    }
+    
+    
+}
---
+++ src/ruletasuerte/RuletaSuerte.java
@@ -0,0 +1,20 @@
+package ruletasuerte;
+
+/**
+ *
+ * @author a22luismsg
+ */
+public class RuletaSuerte {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        for(int i = 0; i < 100; i++) {
+            Ruleta.tirar();
+        }
+    }
+
+}

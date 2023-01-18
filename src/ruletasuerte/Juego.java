8a5b45a003ccea7f06227464b93bb135a8796bed
luis miguel <sempereluismiguel@gmail.com>
2023-01-18 10:06:19 +0100

    estructurando la clase juego

--- src/ruletasuerte/Juego.java
+++ src/ruletasuerte/Juego.java
@@ -3,5 +3,39 @@ package ruletasuerte;
 public class Juego {
     
 
-    public void 
-}
+    public void main() {
+
+        
+
+    } 
+
+    private void mostrarMenu() {
+
+        int opt = 0;
+        do {
+            System.out.println("1.- Jugar");
+            System.out.println("2.- Añadir Jugadores");
+            System.out.println("3.- Salir");
+
+            switch (opt) {
+                case 1:
+                    jugar();
+                    break;
+                case 2:
+                    jugadores();
+                    break;
+                case 3:
+                    System.out.println("Adiós :)");
+            }
+
+        } while ( opt != 3 );
+    }
+
+    private void jugar() {
+
+    }
+
+    private void jugadores () {
+        
+    }
+}   
